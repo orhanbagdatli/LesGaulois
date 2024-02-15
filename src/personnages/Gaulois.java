74318419561027b3fd08794e26419f518a0b28f6
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2024-02-15 15:14:08 +0100

    base de projet avec la class Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,4 +2,29 @@ package personnages;
 
 public class Gaulois {
 
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		
+
+		}
+
+	}
+
 }
