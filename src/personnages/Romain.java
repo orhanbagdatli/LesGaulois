97c876e89ed362a28a24dc0068ee9de87eba46ac
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2024-03-05 17:43:07 +0100

    TP2 invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,30 +7,39 @@ public class Romain {
 	
 	
 	public Romain(String nom, int force) {
-		this.nom = nom; this.force = force;
+		this.nom = nom; 
+		assert(force>0): "force doit etre positif";
+		this.force = force;
+		
+				
+		
 		} 
 	public String getNom() { 
 		return nom; 
 		} 
 	public void parler(String texte) { 
-		System.out.println(prendreParole() + "� " + texte + "�"); 
+		System.out.println(prendreParole() + "'" + texte + "'"); 
 		} 
 	private String prendreParole() {
-		return "Le romain " + nom + " : "; 
+		return "Le romain " + nom + " :"; 
 		}
 	public void recevoirCoup(int forceCoup) {
+		assert (force >= 0) : "force doit etre positif";
+		int ancienneForce = force;
+		
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie"); } 
 		else { 
 			parler("J'abandonne...");
 			} 
+		assert force < ancienneForce : "La force n'a pas diminue";
 		}
 	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
-		System.out.println(minus.prendreParole());
-		minus.parler("metti");
+		
+		
 		minus.recevoirCoup(6);
 		
 	}
