60c576420917b2f210f13986b06f3eadb629dcc7
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2024-04-09 13:46:05 +0200

    les énumérés

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public enum Equipement {
+	CASQUE("casque"),
+	BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	
+	private Equipement(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return nom;
+	}
+
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -4,14 +4,17 @@ public class Romain {
 
 	private String nom;
 	private int force; 
+	private Equipement[] equipements;
+	private int nbEquipement;
 	
 	
 	public Romain(String nom, int force) {
 		this.nom = nom; 
 		assert(force>0): "force doit etre positif";
 		this.force = force;
-		
-				
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
+			
 		
 		} 
 	public String getNom() { 
@@ -36,11 +39,47 @@ public class Romain {
 		assert force < ancienneForce : "La force n'a pas diminue";
 		}
 	
+	 public void sEquiper(Equipement equipement) {
+	        switch (nbEquipement) {
+	            case 0:
+	                ajouterEquipement(equipement);
+	                break;
+	            case 1:
+	                if (equipements[0] == equipement) {
+	                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement.getNom() + " !");
+	                } else {
+	                    ajouterEquipement(equipement);
+	                }
+	                break;
+	            case 2:
+	                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+	                break;
+	            default:
+	                System.out.println("Erreur : nombre d'équipements invalide !");
+	        }
+	    }
+
+	    private void ajouterEquipement(Equipement equipement) {
+	        equipements[nbEquipement] = equipement;
+	        nbEquipement++;
+	        System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.getNom() + ".");
+	    }
+
+	
 	public static void main(String[] args) {
-		Romain minus = new Romain("Minus", 6);
+		Equipement monEquipement = Equipement.CASQUE;
+        System.out.println("Equipement : " + monEquipement);
+
+        monEquipement = Equipement.BOUCLIER;
+        System.out.println("Equipement : " + monEquipement);
 		
-		
+        Romain minus = new Romain("Minus", 6);
 		minus.recevoirCoup(6);
+		minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.BOUCLIER);
+        minus.sEquiper(Equipement.CASQUE);
+		
 		
 	}
 
